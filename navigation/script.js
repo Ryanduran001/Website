19dd12e62482f9c9e6450511181263245fd57e3a
Ryan Duran <112416371+Ryanduran001@users.noreply.github.com>
2023-04-26 17:31:29 +0000

    expanind update

--- navigation/script.js
+++ navigation/script.js
@@ -2,10 +2,10 @@ const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
-open.addEventlistener('click', () => {
+open.addEventListener('click', () => {
     container.classList.add('show-nav')
 })
 
-open.addEventlistener('click', () => {
+close.addEventListener('click', () => {
     container.classList.remove('show-nav')
 })
